a525b7f92a54733814345048c78c7d967023f4aa
4nd1g3t <andresfd@gmail.com>
2025-08-22 10:36:43 -0600

    feat(menu): Prueba Menu

---
+++ app/menu/page.tsx
@@ -0,0 +1,25 @@
+// app/(menu)/menu/[id]/page.tsx
+import PreparePanel  from '@/components/menu/MenuForm';
+import { API_URL } from '@/lib/constants';
+import { getIdTokenOrRedirect } from "@/lib/auth-session";
+
+/*type PageProps = {
+  params: Promise<{ id: string }>; // 👈 Next 15: params como Promise
+};*/
+
+export default async function MenuPage({ params }: PageProps) {
+  const id = "1755879607351-36s1ljldcib" 
+  const idToken = await getIdTokenOrRedirect();
+  return (
+    <main className="p-6">
+      {/* ...tu header + vista del menú... */}
+      <PreparePanel
+        menuId={id}
+        idToken={idToken}
+        apiUrl={API_URL}
+        onAfterConfirm={() => {
+        }}
+      />
+    </main>
+  );
+}
